cc57345e120740104a9441e8a811428f322d8199
sunghyuk930108 <harlie0108@gamil.com>
2024-04-18 09:03:05 +0900

    feat: add alarm clock to baekjoon
    Signed-off-by: sunghyuk930108 <harlie0108@gamil.com>

--- baekjoon/alarm_clock_2884/app.js
+++ baekjoon/alarm_clock_2884/app.js
@@ -1,11 +1,23 @@
 // fs module 방식 / 백준 경로 '/dev/stdin'
 const fs = require("fs");
 
-const [a, b] = fs.readFileSync("./input.txt").toString().split(" ");
+const [a, b] = fs.readFileSync("../input.txt").toString().split(" ");
 
-const x = Number(a);
-const y = Number(b);
+// M을 기준으로 H가 변하는 관계를 잘 생각해야될 듯
+const H = Number(a);
+const M = Number(b);
 
+if (M < 45) {
+    if (H === 0) {
+        console.log(H + 23, 60 - 45 + M);
+    } else {
+        console.log(H - 1, 60 - 45 + M);
+    }
+} else {
+    console.log(H, M - 45);
+}
+
+console.log(H, M);
 /* readline 모듈 방식 **/
 /*
 const readline = require("readline");
