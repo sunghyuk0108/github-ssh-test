03ec6fd390f862bba74cfad1e244ce625b0480d9
sunghyuk930108 <harlie0108@gmail.com>
2024-04-26 09:30:54 +0900

    fix: refactor alarm_clock_2884

--- baekjoon/alarm_clock_2884/app.js
+++ baekjoon/alarm_clock_2884/app.js
@@ -7,11 +7,13 @@ const [a, b] = fs.readFileSync("../input.txt").toString().split(" ");
 const H = Number(a);
 const M = Number(b);
 
+// M이 45보다 작은 경우와 큰 경우 출력되는 값을 생각하고 시간이 0이하일 경우를 생각하면될 듯.
+// 다른 방법으로는 시간을 아예 분으로 환산하고 진행해도 될 듯.
 if (M < 45) {
     if (H === 0) {
-        console.log(H + 23, 60 - 45 + M);
+        console.log(H + 23, 60 + M - 45);
     } else {
-        console.log(H - 1, 60 - 45 + M);
+        console.log(H - 1, 60 + M - 45);
     }
 } else {
     console.log(H, M - 45);
